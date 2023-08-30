18091545a718bc4d88d9ee92f9054c6cc66a66a5
dpere122 <dpere381@fiu.edu>
2023-08-29 20:39:09 -0400

    Added basic window creation with SDL2

---
+++ makefile
@@ -0,0 +1,2 @@
+make: src/main.cpp
+	c++ -o program src/main.cpp
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <string>
+#include <SDL.h>
+#include <stdio.h>
 
 using namespace std;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
-int main()
+int main(int argc, char* args[])
 {
-    cout << "Hello World" << endl;
+    //Window to be rendered to:
+    SDL_Window* window = NULL;
+
+    //The Surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf("SDL could not initialize! SDL Error %s\n", SDL_GetError());
+    }else{
+        window = SDL_CreateWindow("Simple Render Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
+        if(window ==  NULL){
+            printf("Window could not be created! SDL_ERROR: %s\n",SDL_GetError());
+        }else{
+            //Get window Surface
+            screenSurface = SDL_GetWindowSurface(window);
+            //fill the surface white
+            SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface ->format,0xFF,0xFF,0xFF));
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
+            //Hack the window to stay open
+            SDL_Event e; bool quit = false; while(quit == false){while(SDL_PollEvent(&e)){if(e.type == SDL_QUIT) quit = true;}}
+
+        }
+    }
+
     return 0;
 }
